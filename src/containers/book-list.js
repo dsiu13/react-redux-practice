5885db1e5b3eb748070065df7f741a90fba83120
IamZombiez <dereksiu216@gmail.com>
2018-06-12 17:13:57 -0700

    actions

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,15 +1,21 @@
 // A container is component that has direct access to the state created by redux
-// Only through react-redux can the two communicate
-// Most parent component should be a container
+// Only through react-redux can react & redux communicate
+// the most parent component should be a container
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends React.Component {
   renderList(){
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -30,4 +36,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned will end up as props on BookList container
+function mapDispatchToProps(dispatch) {
+  // When select book is called, the result should be passed to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// PromoteBookList from a component to a container. Needs to know
+// about new dispatch method. Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
