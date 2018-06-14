8dcafc966cf7b691459b6ca774c8cfcf2577853c
IamZombiez <dereksiu216@gmail.com>
2018-06-14 09:35:39 -0700

    forgot return state in reducer_weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,11 +1,11 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 // We always want to return a new state instead
-// we NEVER want to manipulate our state
+// NEVER want to manipulate our state
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state];
   }
-
+    return state;
 }
