d8679231d8ae05c7ca90fd7e18af2a4cf76c1e91
IamZombiez <dereksiu216@gmail.com>
2018-06-14 11:35:35 -0700

    save point

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from "../components/chart";
 
 class WeatherList extends React.Component {
 
   renderWeather(cityData) {
 
     const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => temp * 1.8 - 459.67);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
         <tr key={name}>
           <td>{name}</td>
+          <td><Chart data={temps} color="red" units="F"/></td>
+          <td><Chart data={pressures} color="purple" units="hPa" /></td>
+          <td><Chart data={humidities} color="blue" units="%" /></td>
         </tr>
     );
   }
@@ -20,15 +27,14 @@ class WeatherList extends React.Component {
       <thead>
         <tr>
           <th>City</th>
-          <th>Temp</th>
-          <th>Pressure</th>
-          <th>Humidity</th>
+          <th>Temperature (F)</th>
+          <th>Pressure (hPa)</th>
+          <th>Humidity (%)</th>
         </tr>
       </thead>
       <tbody>
         {this.props.weather.map(this.renderWeather)}
       </tbody>
-
       </table>
     );
   };
